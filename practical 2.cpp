59b0a9ccb6a4dff1cd42aabf154ee862e28310d3
janhavee singh <janhavee2005@gmail.com>
2026-02-10 22:27:56 +0530

    Restore bubble, insertion, and selection sort implementations

--- practical 2.cpp
+++ practical 2.cpp
@@ -25,58 +25,59 @@ int main() {
 }
 
 
-////insertion sort
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//    int n, a[10];
-//    cout << "enter the no. of elements:";
-//    cin >> n;
-//    cout << "enter the elements:";
-//    for (int i = 0; i < n; i++)
-//        cin >> a[i];
-//    for (int i = 1; i < n; i++) {
-//        int key = a[i];
-//        int j = i - 1;
-//        while (j >= 0 && a[j] > key) {
-//            a[j + 1] = a[j];
-//            j--;
-//        }
-//        a[j + 1] = key;
-//    }
-//    cout << "sorted array:";
-//    for (int i = 0; i < n; i++)
-//        cout << a[i] << " ";
-//    return 0;
-//}
+//insertion sort
+#include <iostream>
+using namespace std;
+
+int main() {
+   int n, a[10];
+   cout << "enter the no. of elements:";
+   cin >> n;
+   cout << "enter the elements:";
+   for (int i = 0; i < n; i++)
+       cin >> a[i];
+   for (int i = 1; i < n; i++) {
+       int key = a[i];
+       int j = i - 1;
+       while (j >= 0 && a[j] > key) {
+           a[j + 1] = a[j];
+           j--;
+       }
+       a[j + 1] = key;
+   }
+   cout << "sorted array:";
+   for (int i = 0; i < n; i++)
+       cout << a[i] << " ";
+   return 0;
+}
 
 
-////selection sort
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//    int n, a[10];
-//    cout << "enter the no. of elements:";
-//    cin >> n;
-//    cout << "enter the elements:";
-//    for (int i = 0; i < n; i++)
-//        cin >> a[i];
-//    for (int i=0; i<n-1; i++){
-//        int min=i;
-//        for(int j=i+1; j<n; j++){
-//            if(a[j]<a[min])
-//            min=j;
-//        }
-//        int temp=a[i];
-//        a[i]=a[min];
-//        a[min]=temp;
-//    }
-//        cout << "sorted array:";
-//    for (int i = 0; i < n; i++)
-//        cout << a[i] << " ";
-//    return 0;
-//}
+//selection sort
+#include <iostream>
+using namespace std;
+
+int main() {
+   int n, a[10];
+   cout << "enter the no. of elements:";
+   cin >> n;
+   cout << "enter the elements:";
+   for (int i = 0; i < n; i++)
+       cin >> a[i];
+   for (int i=0; i<n-1; i++){
+       int min=i;
+       for(int j=i+1; j<n; j++){
+           if(a[j]<a[min])
+           min=j;
+       }
+       int temp=a[i];
+       a[i]=a[min];
+       a[min]=temp;
+   }
+       cout << "sorted array:";
+   for (int i = 0; i < n; i++)
+       cout << a[i] << " ";
+   return 0;
+}
+
 
 
