5e9cfd4eda6697b6806c8454b57f47160e646f90
janhavee singh <janhavee2005@gmail.com>
2026-03-24 10:18:47 +0530

    updated

--- practical 7.cpp
+++ practical 7.cpp
@@ -1,74 +1,74 @@
-//// String Matching
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//
-//int main() {
-//    string text, pattern;
-//
-//    cout << "Enter text: ";
-//    cin >> text;
-//
-//    cout << "Enter pattern: ";
-//    cin >> pattern;
-//
-//    int n = text.length();
-//    int m = pattern.length();
-//    bool found = false;
-//
-//    for(int i = 0; i <= n - m; i++) {
-//        int j;
-//        for(j = 0; j < m; j++) {
-//            if(text[i + j] != pattern[j])
-//                break;
-//        }
-//        if(j == m) {
-//            cout << "Pattern found at position " << i << endl;
-//            found = true;
-//        }
-//    }
-//
-//    if(!found)
-//        cout << "Pattern not found";
-//
-//    return 0;
-//}
-//
-//
-//// Subset Sum Problem
-//#include <iostream>
-//using namespace std;
-//
-//int n, target;
-//int arr[10];
-//
-//void subsetSum(int index, int sum) {
-//    if(sum == target) {
-//        cout << "Subset found\n";
-//        return;
-//    }
-//
-//    if(index == n || sum > target)
-//        return;
-//
-//    subsetSum(index + 1, sum + arr[index]);
-//    subsetSum(index + 1, sum);
-//}
-//
-//int main() {
-//    cout << "Enter number of elements: ";
-//    cin >> n;
-//
-//    cout << "Enter elements:\n";
-//    for(int i = 0; i < n; i++)
-//        cin >> arr[i];
-//
-//    cout << "Enter target sum: ";
-//    cin >> target;
-//
-//    subsetSum(0, 0);
-//    return 0;
-//}
+// String Matching
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string text, pattern;
+
+   cout << "Enter text: ";
+   cin >> text;
+
+   cout << "Enter pattern: ";
+   cin >> pattern;
+
+   int n = text.length();
+   int m = pattern.length();
+   bool found = false;
+
+   for(int i = 0; i <= n - m; i++) {
+       int j;
+       for(j = 0; j < m; j++) {
+           if(text[i + j] != pattern[j])
+               break;
+       }
+       if(j == m) {
+           cout << "Pattern found at position " << i << endl;
+           found = true;
+       }
+   }
+
+   if(!found)
+       cout << "Pattern not found";
+
+   return 0;
+}
+
+
+// Subset Sum Problem
+#include <iostream>
+using namespace std;
+
+int n, target;
+int arr[10];
+
+void subsetSum(int index, int sum) {
+   if(sum == target) {
+       cout << "Subset found\n";
+       return;
+   }
+
+   if(index == n || sum > target)
+       return;
+
+   subsetSum(index + 1, sum + arr[index]);
+   subsetSum(index + 1, sum);
+}
+
+int main() {
+   cout << "Enter number of elements: ";
+   cin >> n;
+
+   cout << "Enter elements:\n";
+   for(int i = 0; i < n; i++)
+       cin >> arr[i];
+
+   cout << "Enter target sum: ";
+   cin >> target;
+
+   subsetSum(0, 0);
+   return 0;
+}
 
 
 // 8-Queens Problem
